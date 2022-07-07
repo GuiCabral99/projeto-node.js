992737ed8ee5e489f3f85a8a660db264ba33ef2c
Guilherme Cabral <gui.cabral7641@gmail.com>
2022-07-07 00:06:17 -0300

    criação do  bd e da pasta src

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const {Person} = require('./person')
-const person = new Person("Julia");
+// const {Person} = require('./person')
+
+const dotenv = require('dotenv');
+const connectToDatabase = require('./src/database/connect');
+dotenv.config()
+
+connectToDatabase()
+
+// const person = new Person("Julia");
 
 // require('./modules/path')
 // require('./modules/fs')
 // require('./modules/http.js')
-require('./modules/express')
+// require('./modules/express')
